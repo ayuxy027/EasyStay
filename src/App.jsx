ac151718306023d7325fb7bce55409c30951def7
Ayush Yadav <ayush421301@gmail.com>
2024-06-17 12:30:28 +0530

    Splitted Navbar Component to resuse

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import SignUp from "./components/SignUp";
 import SearchBar from "./components/SearchBar";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+    <NavBar />
     <SignUp />
     <SearchBar />
     </>
