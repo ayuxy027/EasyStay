b7fb15248f36c964f9edd5f4265fccde722c0c5a
Ayush Yadav <ayush421301@gmail.com>
2024-06-18 08:22:58 +0530

    Added Login Route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar.jsx";
 import HomePage from "./pages/HomePage.jsx"; 
 import SignUp from "./components/SignUp.jsx";
 import { Route,Routes } from "react-router-dom";
+import LogIn from "./components/LogIn.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NavBar/> 
       <Routes>
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<LogIn />} />
       <Route path="/home" element={<HomePage/>}/>
       </Routes>
     </>
