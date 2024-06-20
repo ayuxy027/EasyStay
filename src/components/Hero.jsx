57d61f60a7b73d53cd1d4b935a91b8b0863f0dfb
Ayush Yadav <ayush421301@gmail.com>
2024-06-20 14:15:23 +0530

    Removed "Budget" word from Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ function Hero() {
                         <div className="grid max-w-lg grid-cols-1 mx-auto lg:max-w-full lg:items-center lg:grid-cols-2 gap-y-12 lg:gap-x-8">
                             <div>
                                 <div className="text-center lg:text-left">
-                                    <h1 className="text-4xl font-bold leading-tight text-transparent text-gray-900 bg-gradient-to-r from-rose-700 to-amber-500 bg-clip-text sm:text-5xl sm:leading-tight lg:leading-tight lg:text-6xl font-pj">India’s Leading Budget Hotels Chain</h1>
+                                    <h1 className="text-4xl font-bold leading-tight text-transparent text-gray-900 bg-gradient-to-r from-rose-700 to-amber-500 bg-clip-text sm:text-5xl sm:leading-tight lg:leading-tight lg:text-6xl font-pj">India’s Leading Hotels Chain</h1>
                                     <p className="mt-2 text-lg text-gray-600 sm:mt-8 font-inter">Enjoy a perfect vacation at any holiday destination with our affordable and flexible hourly hotels.</p>
                                 </div>
                                 <div className="flex items-center justify-center mt-10 space-x-6 lg:justify-start sm:space-x-8">
