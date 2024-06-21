7e6ae36a7aa066d5cfc16a2712cc01fbcdafcf54
Himanshu Rai <himanshuraimau9@gmail.com>
2024-06-21 21:55:45 +0530

    change color and font of contact us text

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -1,37 +1,45 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Support = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    message: ''
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
   });
 
   const handleChange = (e) => {
     const { id, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
-      [id]: value
+      [id]: value,
     }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted:', formData);
+    console.log("Form submitted:", formData);
   };
 
   return (
     <div className="bg-white">
-      <div className="max-w-5xl px-4 py-10 mx-auto xl:px-0 lg:py-20">
-        <div className="max-w-3xl mb-10 lg:mb-14">
-          <h2 className="text-2xl font-semibold text-black md:text-4xl md:leading-tight">Contact Us</h2>
-          <p className="mt-1 text-gray-600">We're here to assist you with your stay.</p>
-        </div>
+      <div className="max-w-3xl mt-10 mx-auto text-center">
+        <h2
+          className="text-3xl font-bold leading-tight text-transparent
+                     text-black sm:text-4xl lg:text-5xl 
+                     bg-proj bg-clip-text"
+        >
+          Contact Us
+        </h2>
+        <p className="mt-1 leading-tight  text-black text-transparent bg-proj bg-clip-text">
+          We're here to assist you with your stay.
+        </p>
+      </div>
 
+      <div className="max-w-5xl px-4 py-6 mx-auto xl:px-0 lg:py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-x-10 lg:gap-x-16">
-          <div className="pb-10 mb-10 border-b border-gray-300 md:order-2 md:border-b-0 md:pb-0 md:mb-0">
+          <div className="pb-6 mb-6 border-b border-gray-300 md:order-2 md:border-b-0 md:pb-0 md:mb-0">
             <form onSubmit={handleSubmit}>
               <div className="space-y-4">
                 <div className="relative">
@@ -162,7 +170,7 @@ const Support = () => {
                 <p className="mt-5">
                   <button
                     type="submit"
-                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-gray-800 rounded-full group gap-x-2 focus:outline-none"
+                    className="text-white bg-proj transition-transform duration-200 ease-in-out hover:scale-105 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full px-3 py-2 inline-flex items-center gap-x-2"
                   >
                     Submit
                     <svg
@@ -188,22 +196,36 @@ const Support = () => {
 
           <div className="space-y-14">
             <div className="flex gap-x-5">
-              <svg className="flex-shrink-0 text-gray-800 h-7 w-7" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <svg
+                className="flex-shrink-0 text-gray-800 h-7 w-7"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
                 <path d="M21 10c0 6-9 12-9 12S3 16 3 10a9 9 0 1 1 18 0z" />
                 <circle cx="12" cy="10" r="3" />
               </svg>
               <div>
                 <h3 className="text-lg font-medium text-gray-800">Address</h3>
                 <p className="text-gray-600">
-                  Sarhaul,Sec-18,
-                  Gurgaon,
-                  Haryana, 122001
-                  </p>
+                  Sarhaul,Sec-18, Gurgaon, Haryana, 122001
+                </p>
               </div>
             </div>
 
             <div className="flex gap-x-5">
-              <svg className="flex-shrink-0 text-gray-800 h-7 w-7" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <svg
+                className="flex-shrink-0 text-gray-800 h-7 w-7"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
                 <path d="M21 8V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2" />
                 <path d="M21 16v2a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2" />
                 <path d="M3 12h18" />
@@ -211,13 +233,20 @@ const Support = () => {
               </svg>
               <div>
                 <h3 className="text-lg font-medium text-gray-800">Email</h3>
-                <p className="text-gray-600">
-                  Support@idbookhotels.com</p>
+                <p className="text-gray-600">Support@idbookhotels.com</p>
               </div>
             </div>
 
             <div className="flex gap-x-5">
-              <svg className="flex-shrink-0 text-gray-800 h-7 w-7" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <svg
+                className="flex-shrink-0 text-gray-800 h-7 w-7"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
                 <path d="M22 16.92V21a2 2 0 0 1-2.18 2A19.83 19.83 0 0 1 3 5.18 2 2 0 0 1 5 3h4.09a2 2 0 0 1 2 1.72c.12 1 .51 2.05 1.09 2.92a2 2 0 0 1-.45 2.32L10 12.54a16 16 0 0 0 6.09 6.09l2.58-2.58a2 2 0 0 1 2.32-.45 11.55 11.55 0 0 0 2.92 1.09 2 2 0 0 1 1.72 2.04z" />
               </svg>
               <div>
@@ -229,7 +258,12 @@ const Support = () => {
         </div>
 
         <div className="mt-10 text-center">
-          <Link to="/faq" className="text-sm underline text-sky-600 md:text-base">See FAQs</Link>
+          <Link
+            to="/faq"
+            className="text-sm underline text-sky-600 md:text-base"
+          >
+            See FAQs
+          </Link>
         </div>
       </div>
     </div>
